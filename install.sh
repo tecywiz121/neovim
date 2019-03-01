6feeb568a080053a27c06b9ed4a6963b361de30f
Sam Wilson <sawilson@akamai.com>
2019-03-01 11:56:26 -0500

    Don't recreate symlinks if they already exist

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -euf -o pipefail
 
+symlink() {
+    local TARGET="$1"
+    local LINK_NAME="$2"
+
+    local REAL_TARGET="$(readlink -f -n "$TARGET")"
+
+    if ! [ -h "$LINK_NAME" -a "$(readlink -f -n "$LINK_NAME")" = "$REAL_TARGET" ]; then
+        ln -s "$TARGET" "$LINK_NAME"
+    fi
+}
+
 # Change into the directory the script is located in to make things easier
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
@@ -9,5 +20,5 @@ cd "$SCRIPT_DIR"
 git submodule update --init --recursive
 
 # Create symbolic links to the nvim configuration folders.
-ln -s "$SCRIPT_DIR/config" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
-ln -s "$SCRIPT_DIR/share"  "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site"
+symlink "$SCRIPT_DIR/config" "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+symlink "$SCRIPT_DIR/share"  "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site"
